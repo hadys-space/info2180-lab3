ffaec5849d428fa469764dd4f4445379eb027c4e
hadys.space <hadijones77@gmail.com>
2024-10-24 23:36:23 -0500

    Added X or O when clicked

--- tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
+++ tic-tac-toe-master/tic-tac-toe-master/tic-tac-toe.js
@@ -1,7 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(){
     const squares = document.querySelectorAll('#board div');
+    let boardSlots = ['','','','','','','','',''];
+    let player = ["X", "O"];
+    let playerIndex = 0; 
 
-    squares.forEach(square => {
+    squares.forEach((square, index) => {
         square.classList.add('square');
-    })
+
+        square.addEventListener('click',function(e){
+            if(boardSlots[index] === ''){
+                boardSlots[index] = player[playerIndex];
+
+                square.textContent = player[playerIndex];
+
+                square.classList.add(player[playerIndex]);
+
+                playerIndex = playerIndex === 0 ? 1:0;
+
+            }
+    
+        });
+    });
 });
+
